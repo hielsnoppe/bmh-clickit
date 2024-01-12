2c3b61213e664e5cc135e45b667a4099a3fc5dca
Niels Hoppe <mail@nielshoppe.de>
2024-01-12 11:24:23 +0100

    Add event listener to playarea

--- docs/clickit.js
+++ docs/clickit.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log('Hello World!');
+
+    const e_target = document.getElementById('target');
+    const e_playarea = document.getElementById('playarea');
+
+    e_playarea.addEventListener('click', (event) => {
+        console.log(event.target === e_target ? 'Treffer!' : 'Daneben!');
+    });
 });
