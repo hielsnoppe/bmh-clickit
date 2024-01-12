47c435adad034b8c4ad80cde7b0b01a0caaf96bf
Niels Hoppe <mail@nielshoppe.de>
2024-01-12 11:51:34 +0100

    Move target on click

--- docs/clickit.js
+++ docs/clickit.js
@@ -4,7 +4,22 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const e_target = document.getElementById('target');
     const e_playarea = document.getElementById('playarea');
 
+    const target = {
+        width: 200,
+        height: 150
+    }
+    const playarea = {
+        width: 800,
+        height: 600
+    }
+
     e_playarea.addEventListener('click', (event) => {
         console.log(event.target === e_target ? 'Treffer!' : 'Daneben!');
+        
+        const top = Math.random() * (playarea.height - target.height);
+        const left = Math.random() * (playarea.width - target.width);
+
+        e_target.style.top = `${top}px`;
+        e_target.style.left = `${left}px`;
     });
 });
